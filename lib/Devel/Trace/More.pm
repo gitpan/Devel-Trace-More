c281cdbf85ceddbbd8709111921e584db03019db
Mike Burns <mburns.lungching@gmail.com>
2010-04-02 18:02:16 +0000

    Import of LUNGCHING/Devel-Trace-More-0.01 from CPAN.
    gitpan-cpan-distribution: Devel-Trace-More
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         LUNGCHING/Devel-Trace-More-0.01.tar.gz
    gitpan-cpan-author:       LUNGCHING
    gitpan-cpan-maturity:     released

--- lib/Devel/Trace/More.pm
+++ lib/Devel/Trace/More.pm
@@ -1,6 +1,5 @@
 package Devel::Trace::More;
 
-use 5.010000;
 use strict;
 use warnings;
 
@@ -9,41 +8,48 @@ use Exporter;
 use base 'Exporter';
 our @EXPORT_OK = qw{ trace filter_on };
 
-our $VERSION = '0.01_01';
+our $VERSION = '0.01';
 
 our $IS_INTERESTING = sub { return 1; };
 our $TRACE = 1;
 
 # This is the important part.  The rest is just fluff.
 sub DB::DB {
-  return unless $TRACE;
-  my ($p, $f, $l) = caller;
-
-  # have no idea how to do this with strict on
-  no strict 'refs';
-  my $code = \@{"::_<$f"};
-  use strict 'refs';
-  my $code_line = $code->[$l] ? $code->[$l] : '';
-  chomp($code_line);
-
-  print STDERR ">> $f:$l: $code_line\n" if $IS_INTERESTING->($f, $code_line);
+    return unless $TRACE;
+    my ($p, $f, $l) = caller;
+  
+    # have no idea how to do this with strict on
+    no strict 'refs';
+    my $code = \@{"::_<$f"};
+    use strict 'refs';
+    my $code_line = defined($code->[$l]) ? $code->[$l] : '';
+    chomp($code_line);
+  
+    print STDERR ">> $f:$l: $code_line\n" if $IS_INTERESTING->($p, $f, $l, $code_line);
 }
 
 sub filter_on {
     my $filter = shift;
 
-    if (! ref($filter)) {
-        $filter = qr/$filter/;
+    if ( uc( ref($filter) ) eq 'REGEXP') {
+        $IS_INTERESTING = sub { my ($p, $file, $line_num, $code_line) = @_; return $file =~ $filter || $code_line =~ $filter; };
     }
-
-    $IS_INTERESTING = sub { my ($f, $l) = @_; return $f =~ $filter || $l =~ $filter; } if ref($filter) eq 'Regexp';
+    elsif ( uc( ref($filter) ) eq 'CODE') {
+        $IS_INTERESTING = $filter;
+    }
+    elsif (! ref($filter) ) {
+        $IS_INTERESTING = sub { my ($p, $file, $line_num, $code_line) = @_; return ( index($file, $filter) > -1) || ( index($code_line, $filter) > -1); };
+    }
+    else {
+        die "I don't know how to handle that filter!";
+    }
 }
 
 my %tracearg = ('on' => 1, 'off' => 0);
 sub trace {
-  my $arg = shift;
-  $arg = $tracearg{$arg} while exists $tracearg{$arg};
-  $TRACE = $arg;
+    my $arg = shift;
+    $arg = $tracearg{$arg} while exists $tracearg{$arg};
+    $TRACE = $arg;
 }
 
 1;
@@ -61,15 +67,33 @@ Devel::Trace::More - Like Devel::Trace but with more control
   
   filter_on('blah');
 
+  filter_on(qr/blah/);
+
+  filter_on(sub { my ($p, $file, $line, $code) = @_; ... });
+
   # or
 
-  filter_on(qr/blah/);
+  $IS_INTERESTING = sub { my ($p, $file, $line, $code) = @_; ... };
 
 =head1 DESCRIPTION
 
-For now look at documentation for L<Devel::Trace>.
-More features to come!
+This module will print out every line of code as it executes when used under
+the perl debugger.  By default all executed lines will print to STDERR. By 
+calling filter_on with a code ref, regex ref, a scalar, or by setting 
+$Devel::Trace::More::IS_INTERESTING directly then only those lines that are
+'interesting' will be returned.
 
+If filter_on is given a scalar or a regular expression reference then the
+file name of the code being executed or the line of code itself that matches
+the given patter will be printed.  Passing in a code ref is the same as setting
+$IS_INTERESTING itself.  Setting the filter this way will allow you to do
+more complicated things like filtering on just the module name or the line number
+of the code.  $IS_INTERESTING can be changed in different places in the code if
+needed.
+
+Caveat: Using regular epressions to filter what gets printed can cause unexected
+issues if the code being debugged relies on the regular expression global variables.
+Use with caution!
 
 =head1 SEE ALSO
 
--- t/Devel-Trace-More.t
+++ t/Devel-Trace-More.t
@@ -5,11 +5,21 @@
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 1;
+use Test::More tests => 8;
 BEGIN { use_ok('Devel::Trace::More') };
 
 #########################
 
-# Insert your test code below, the Test::More module is use()ed here so read
-# its man page ( perldoc Test::More ) for help writing this test script.
+is( $Devel::Trace::More::IS_INTERESTING->(), 1, "Default IS_INTERESTING returns 1 with no params entered");
 
+Devel::Trace::More::filter_on(qr/Test/);
+is( $Devel::Trace::More::IS_INTERESTING->('','Stuff Test Blah', '',''),  1,  "IS_INTERESTING found something interesting with a regex");
+is( $Devel::Trace::More::IS_INTERESTING->('','Stuff Thing Blah', '',''), '', "IS_INTERESTING did not find something interesting with a regex");
+
+Devel::Trace::More::filter_on( sub { my ($p, $f, $l, $c) = @_; return index($f, 'Test') > -1; } );
+is( $Devel::Trace::More::IS_INTERESTING->('','Stuff Test Blah', '',''),  1,  "IS_INTERESTING found something interesting with a code ref");
+is( $Devel::Trace::More::IS_INTERESTING->('','Stuff Thing Blah', '',''), '', "IS_INTERESTING did not find something interesting with a code ref");
+
+Devel::Trace::More::filter_on('Test');
+is( $Devel::Trace::More::IS_INTERESTING->('','Stuff Test Blah', '',''),  1,  "IS_INTERESTING found something interesting with a scalar");
+is( $Devel::Trace::More::IS_INTERESTING->('','Stuff Thing Blah', '',''), '', "IS_INTERESTING did not find something interesting with a scalar");
